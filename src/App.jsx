2b08b8b054991f048005a57dd0bed2f35be5f886
Woldamlak Debasu Teshale <129512486+WoldamlakDebasu@users.noreply.github.com>
2024-04-17 11:38:59 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -10,24 +10,6 @@ function App() {
   const [allRoutes, setAllRoutes] = useState([...publicRoutes])
 
   useEffect(() => {
-    const fetchData = async () => {
-      let token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2MGEzZjM1NTFiNzAxMGJlMzBhYjg3YiIsInJvbGUiOiJzZWxsZXIiLCJpYXQiOjE3MTI4ODc2ODksImV4cCI6MTcxMzQ5MjQ4OX0.ww5VeFxypeRzwefqdC-iGcjVdaDk8Isvp_s1Y1JFrDg'
-      try {
-        const response = await fetch('http://localhost:5000/api/get-user', {
-          headers: {
-            Authorization: `Bearer ${token}`
-          }
-        });
-        const data = await response.json();
-        console.log(data, 'data11');
-      } catch (error) {
-        console.log(error, 'fine');
-      }
-    };
-    fetchData();
-  }, []);  
-
-  useEffect(() => {
     const routes = getRoutes()
     setAllRoutes([...allRoutes, routes])
   }, [])
